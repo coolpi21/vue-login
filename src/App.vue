e52b4a2e93f1873b9c040c2dfad6b36255f939df
coolfish <coolpi07@gmail.com>
2019-10-12 12:04:31 +0800

    use vee-validate that add to form validation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,134 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="email"
+              v-validate="'required|email'"
+              placeholder="请输入用户名"
+              class="layui-input"
+              v-model="name"
+            />
+          </div>
+          <div class="error layui-form-mid">
+            <span>{{ errors.first("email") }}</span>
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input
+              type="password"
+              name="password"
+              v-validate="'required|min:6'"
+              placeholder="请输入密码"
+              class="layui-input"
+              v-model="password"
+            />
+          </div>
+          <div class="error layui-form-mid">
+            <span>{{ errors.first("password") }}</span>
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="length"
+              placeholder="请输入验证码"
+              v-validate="{ required: true, length: 4 }"
+              class="layui-input"
+              v-model="code"
+            />
+          </div>
+          <div class="error layui-form-mid">
+            <span>{{ errors.first("length") }}</span>
+          </div>
+          <div class="layui-form-mid svg" v-html="svg" @click="getCaptcha">
+            辅助文字
+          </div>
+        </div>
+        <button type="button" class="layui-btn" @click="checkError">
+          立即登录
+        </button>
+        <a href="http://www.layui.com" class="login-text-hover">忘记密码？</a>
+      </form>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      errorMsg: [],
+      name: '',
+      password: '',
+      code: ''
     }
+  },
+  methods: {
+    getCaptcha () {
+      axios.get('http://localhost:3000/getCaptcha').then(res => {
+        console.log(res)
+        if (res.status === 200) {
+          if (res.data.code === 200) {
+            this.svg = res.data.data
+          }
+        }
+      })
+    },
+    checkError () {
+      this.errorMsg = []
+      if (!this.name) {
+        this.errorMsg.push('用户名为空')
+      }
+      if (!this.password) {
+        this.errorMsg.push('密码为空')
+      }
+      if (!this.code) {
+        this.errorMsg.push('验证码为空')
+      }
+    }
+  },
+  mounted () {
+    this.getCaptcha()
   }
+}
+</script>
+<style lang="scss" scoped>
+#app {
+  background: #f2f2f2;
+}
+
+.layui-container {
+  background: #fff;
+}
+
+input {
+  width: 190px;
+}
+
+.login-text-hover {
+  margin-left: 10px;
+  &:hover {
+    color: #419388;
+  }
+}
+
+.svg {
+  position: relative;
+  top: -15px;
+}
+.error {
+  color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate, { Validator } from 'vee-validate' // 调用表单验证机制
+import './dict/dictionary' // 调用自定义vee-validate字典
+
+Vue.use(VeeValidate)
+const validator = new Validator()
+validator.localize('zh')
 
 Vue.config.productionTip = false
 
